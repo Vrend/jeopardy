8a44ab4502d10c8efab23dd192c35d9ec8f9dad1
John Broderick <johnbroderick12@gmail.com>
2017-05-15 15:43:30 -0400

    need to create images to test

--- Main.py
+++ Main.py
@@ -1,36 +1,73 @@
 from pyprocessing import *
 import random as r
+import os
+import time
 
-state = 4
+state = 0
 
 team1 = ''
 team2 = ''
 score1 = 0
 score2 = 0
 
-boxx = []
-boxy = []
+turn = 0
 
+answer = ''
+
+answer_response = ''
+
+categories = {'Derivatives': 0, 'Integrals': 1, 'Function Analysis': 2, "Misc": 3}
+
+images = {}
+
+image_to_answer = {}
+
+solved = [[False, False, False, False, False], [False, False, False, False, False], [False, False, False, False, False], [False, False, False, False, False], [False, False, False, False, False]]
+
+selected_text_box = False
 
 show_box = False
+
 selected_box = (0, 0)
 
+root = os.getcwd()
+
+tries = 2
+
+stolen = False
+
+def load_problems():
+    global images, categories, image_to_answer
+    problems = open(root+'/problem_list', 'r')
+    while True:
+        l = problems.readline()
+        if l is None:
+            break
+        problem = l.split(',')
+        images[(categories[problem[1]], (int(problem[2])/100-1))] = problems[0]
+        image_to_answer[problem[0]] = int(problem[3])
+
+
 def setup():
+    if not os.path.isdir(root+"/problems"):
+        os.mkdir(root+"/problems")
+    if not os.path.isfile(root+'/problem_list'):
+        problems = open(root+'/problem_list', 'w+')
+        problems.close()
     size(800, 500)
     frameRate(15)
 
 
 def draw():
-    global show_box, selected_box, state
+    global show_box, selected_box, state, answer
     background(58, 57, 253)
 
     if state == 0:
         draw_enter()
 
     if state == 3:
-        if(show_box):
-            print selected_box
-            show_box = False
+        if show_box:
+            state = 4
         strokeWeight(2)
         draw_grid()
         draw_title()
@@ -42,10 +79,82 @@ def draw():
         draw_text_box()
 
 
+def check_answer(num):
+    global selected_box
+    ans = image_to_answer[images[selected_box]]
+    if (ans - .005) <= num <= (ans + .005):
+        return True
+    return False
+
+
+
+def keyPressed():
+    global answer, selected_text_box, state, answer_response, selected_box, show_box, tries, image_to_answer, images, solved, turn, stolen
+    if state == 4:
+        if selected_text_box:
+            if key.code == BACKSPACE or key.code == DELETE:
+                if len(answer) > 0:
+                    answer = answer[0:len(answer)-1]
+            elif key.code == ENTER or key.code == RETURN:
+                if check_answer(answer):
+                    answer_response = 'Correct!'
+                    time.sleep(1)
+                    answer_response = ''
+                    add_points((selected_box[1]+1)*100)
+                    show_box = False
+                    tries = 2
+                    solved[selected_box[0]][selected_box[1]] = True
+                    state = 3
+                    if not stolen:
+                        turn = turn % 2 + 1
+                    else:
+                        stolen = False
+                else:
+                    tries -= 1
+                    if tries == 0:
+                        answer_response = 'The answer was: ' + image_to_answer[images[selected_box]]
+                        tries = 2
+                        time.sleep(1)
+                        show_box = False
+                        solved[selected_box[0]][selected_box[1]] = True
+                        state = 4
+                    else:
+                        answer_response = 'Sorry, that was wrong: but the other team can steal!'
+                        turn = turn % 2 + 1
+                        stolen = True
+                answer = ''
+
+def add_points(num):
+    global turn, score1, score2
+    if turn == 1:
+        score1 += num
+    elif turn == 2:
+        score2 += num
+    else:
+        print "Problem with scoring"
+
+def keyTyped():
+    global answer, state, selected_text_box
+    thing = key.char
+    if state == 4:
+        if selected_text_box:
+            if '0123456789.'.__contains__(thing):
+                answer += thing
+
+
 def draw_text_box():
-    noStroke()
+    global selected_text_box, answer
+    if selected_text_box:
+        stroke(3)
+    else:
+        noStroke()
+
     fill(255)
     rect(340, 375, 100, 50)
+    fill(0)
+    textSize(12)
+    text(answer, 390, 410)
+
 
 def draw_enter():
     fill(255)
@@ -57,13 +166,36 @@ def draw_enter():
     textSize(12)
     text('Enter', 365, 355)
 
+
 def draw_points():
+    global solved
     for a in range(1, 6):
-        x = a/5.0 * 800 - 80
+        x = a / 5.0 * 800 - 80
+
+        if(solved[a-1][0] == True):
+            fill(0, 255, 0)
+        else:
+            fill(255)
         text("100", x, 135)
+        if(solved[a-1][1] == True):
+            fill(0, 255, 0)
+        else:
+            fill(255)
         text("200", x, 220)
+        if(solved[a-1][2] == True):
+            fill(0, 255, 0)
+        else:
+            fill(255)
         text("300", x, 300)
+        if(solved[a-1][3] == True):
+            fill(0, 255, 0)
+        else:
+            fill(255)
         text("400", x, 375)
+        if(solved[a-1][4] == True):
+            fill(0, 255, 0)
+        else:
+            fill(255)
         text("500", x, 465)
 
 
@@ -105,32 +237,40 @@ def draw_grid():
 
 
 def mouseClicked():
-    global state, show_box, selected_box
+    global state, show_box, selected_box, selected_text_box
     x = mouse.x
     y = mouse.y
 
     if state == 0:
-        if x > 340 and x < 440 and y > 325 and y < 375:
+        if 340 < x < 440 and 325 < y < 375:
             state = 3
 
     elif state == 3:
         v1 = None
         v2 = None
         for a in range(1, 6):
-            x1 = (a-1)/5.0 * 800
-            x2 = a/5.0 * 800
-            if x < x2 and x > x1:
-                v1 = a-1
+            x1 = (a - 1) / 5.0 * 800
+            x2 = a / 5.0 * 800
+            if x2 > x > x1:
+                v1 = a - 1
                 break
         for b in range(1, 6):
-            y1 = (b-1)/6.0 * 500
-            y2 = b/6.0 * 500
-            if y < y2 and y > y1:
-                v2 = b-2
+            y1 = (b - 1) / 6.0 * 500
+            y2 = b / 6.0 * 500
+            if y2 > y > y1:
+                v2 = b - 2
                 break
-        if v2 == None:
+        if v2 is None:
             v2 = 4
         show_box = True
         selected_box = (v1, v2)
+        state = 4
+
+    elif state == 4:
+        if 340 < x < 440 and 375 < y < 425:
+            selected_text_box = True
+        else:
+            selected_text_box = False
+
 
 run()
